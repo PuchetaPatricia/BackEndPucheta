f6f7cf6ed46d48f082299c84b15be4caa5227b27
patricia pucheta <ptrcpucheta@gmail.com>
2022-11-01 17:26:07 -0300

    Corregido

---
+++ BackEnd/src/main/java/com/portfolio/PuchetaPatricia/PuchetaPatriciaApplication.java
@@ -0,0 +1,15 @@
+package com.portfolio.PuchetaPatricia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class PuchetaPatriciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuchetaPatriciaApplication.class, args);
+	}
+
+}
